2e34b06a9fb8bcff52b1c72c1f3705158ad35f85
IONUT <IONUT@GoldenBoy>
2013-09-11 23:43:07 +0300

    final result

--- ROSEduT/src/Main.java
+++ ROSEduT/src/Main.java
@@ -1,12 +1,35 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author IONUT
  */
+
 public class Main {
-    int i = 4;
+
+    public static int add_one(int x) {
+        return x + 1;
+    }
+
+    public static int multiply_by_ten(int x) {
+        return x * 100;
+    }
+
+    public static int substract_eleven(int x) {
+        return x - 11;
+    }
+
+    public static int add_fifty(int x) {
+        return x + 50;
+    }
+
+    public static void collection_of_funtions(int x) {
+        System.out.print("[" + add_one(x)+ ", ");
+        System.out.print(add_fifty(x)+ ", ");
+        System.out.print(substract_eleven(x)+ ", ");
+        System.out.println(multiply_by_ten(x)+ "]");
+    }
+
+    public static void main(String[] args) {
+        int i = 4;
+        collection_of_funtions(i);
+    }
 }
